2b5189abd0f9026f3556e6ab0b093c3a9c8c1208
Abdelrhman-Dev <58493502+AbdrhmanDev@users.noreply.github.com>
2025-04-08 14:48:55 +0200

    fixbody

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./Features/components/navbar/navbar.component";
 import { HomeComponent } from "./Features/components/home/home.component";
+import { AboutComponent } from "./Features/components/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
